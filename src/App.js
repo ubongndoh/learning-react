dfee7873a26da592c91426e7e3fecb2a51e81387
Ubong Ndoh <ubongndoh@gmail.com>
2022-03-14 14:16:14 +0100

    i finish implementing the cards

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<Card name='card 2' description='this is card 2' />
 			<Card name='card 3' description='this is card 3' />
 			<Card name='card 4' description='this is card 4' />
-			<Card name='card 5' description='this is card 5' />
+			<Card name='card 5' description='this is card 5444444' />
 		</div>
 	);
 }
